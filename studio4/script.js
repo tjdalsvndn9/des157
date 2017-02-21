ae48ad57f9875a98626a8e74a8790da04dabd61e
Sungmin yi <ssuyi@ucdavis.edu>
2017-02-20 20:21:52 -0800

    Add files via upload

--- studio4/script.js
+++ studio4/script.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function(event){
 	chat.append(message);
 	setTimeout(function(){
 		document.querySelector('.chatlogs').appendChild(chat);
-	},4000);
+	},4000);	
 
 
 
@@ -38,196 +38,106 @@ document.addEventListener("DOMContentLoaded", function(event){
 	secondtext=['wanna hang','Wannna hang?','wanna hang?','yes','yeah','hell yeah'];
 		thirdtext=['Davis','davis','sac'];
 		refusingText= ['no','hell no','I am not a single','im not single','nah'];
-		fourthText = ['what','what?','what do you mean?','what do you mean','what you mean', 'and?','you are married?','alright','okay'];
-		AdminText =['admin Sung','Sung','admin','creater Sung'];
-
 
 	var button = document.getElementById('sendButton');
 
+	
+
+
 	button.addEventListener('click',function(e){
-			e.preventDefault();
-			var chat = document.createElement('div');
-			chat.setAttribute('class','chat1');
+		e.preventDefault();
+	
+		var chat = document.createElement('div');
+		chat.setAttribute('class','chat1');
+		var photo = document.createElement('div');
+		photo.setAttribute('class','user-photo1');
+		chat.appendChild(photo);
+		var textArea = document.getElementById('textArea').value;
+		var textNode = document.createTextNode(textArea);
+		var message = document.createElement('p');
+		message.setAttribute('class','chat-message1');
+		message.appendChild(textNode);
+		chat.appendChild(message);
+		document.querySelector('.chatlogs').appendChild(chat);
+
+		
+			randomNumberGenerator = Math.max(2000,Math.floor(Math.random()*3500));
+	setInterval(function(){
+			console.log('I hope you find your love');
+		},randomNumberGenerator);
 
 
-			var photo = document.createElement('div');
-			photo.setAttribute('class','user-photo1');
-
-			chat.appendChild(photo);
-
-			var textArea = document.getElementById('textArea').value;
-			var textNode = document.createTextNode(textArea);
-			var message = document.createElement('p');
-			message.setAttribute('class','chat-message1');
-			message.appendChild(textNode);
-
-			chat.appendChild(message);
-
-			document.querySelector('.chatlogs').appendChild(chat);
-
-
-	randomNumberGenerator = Math.max(2000,Math.floor(Math.random()*3500));
-
-
-
-					for(var i=0; i<refusingText.length; i++){
-				if(textArea.trim().toLowerCase() == refusingText[i]){
-					setTimeout(function(){
-						var photo = document.createElement('div');
-						photo.setAttribute('class','user-photo');
-						var message = document.createElement('p');
-						message.setAttribute('class','chat-message');
-						friendText = document.createTextNode('you suck')
-						message.appendChild(friendText);
-						var chat = document.createElement('div');
-						chat.setAttribute('class','chat');
-						chat.appendChild(photo);
-						chat.append(message);
-						document.querySelector('.chatlogs').appendChild(chat);
-
-							},2000);
-							var login = document.createElement('p');
-			login.setAttribute('class','login');
-			loginText = document.createTextNode('user99 left the room');
-			login.appendChild(loginText);
-			setTimeout(function(){
-				document.querySelector('.chatlogs').appendChild(login);
-			},3000);
-
-						}
-					}
-
-
-
-					for(var i=0; i<AdminText.length; i++){
-				if(textArea.trim().toLowerCase() == AdminText[i]){
-					setTimeout(function(){
-						var photo = document.createElement('div');
-						photo.setAttribute('class','user-photo');
-						var message = document.createElement('p');
-						message.setAttribute('class','chat-message');
-						friendText = document.createTextNode('you are the best, Sung')
-						message.appendChild(friendText);
-						var chat = document.createElement('div');
-						chat.setAttribute('class','chat');
-						chat.appendChild(photo);
-						chat.append(message);
-						document.querySelector('.chatlogs').appendChild(chat);
-
-							},2000);
-							var login = document.createElement('p');
-			login.setAttribute('class','login');
-			loginText = document.createTextNode('user99 left the room');
-			login.appendChild(loginText);
-			setTimeout(function(){
-				document.querySelector('.chatlogs').appendChild(login);
-			},3000);
-
-						}
-					}
-
-
-
-
-
-			for(var i=0; i<firsttext.length; i++){
-				if(textArea.trim().toLowerCase() == firsttext[i]){
-					setTimeout(function(){
-						var photo = document.createElement('div');
-						photo.setAttribute('class','user-photo');
-						var message = document.createElement('p');
-						message.setAttribute('class','chat-message');
-						friendText = document.createTextNode('free tonight?')
-						message.appendChild(friendText);
-						var chat = document.createElement('div');
-						chat.setAttribute('class','chat');
-						chat.appendChild(photo);
-						chat.append(message);
-						document.querySelector('.chatlogs').appendChild(chat);
-
-					},randomNumberGenerator);
-
-				}
-			}
-
-					for(var i=0; i<secondtext.length; i++){
-				if(textArea.trim().toLowerCase() == secondtext[i]){
-					setTimeout(function(){
-						var photo = document.createElement('div');
-						photo.setAttribute('class','user-photo');
-						var message = document.createElement('p');
-						message.setAttribute('class','chat-message');
-						friendText = document.createTextNode('where do you live?')
-						message.appendChild(friendText);
-						var chat = document.createElement('div');
-						chat.setAttribute('class','chat');
-						chat.appendChild(photo);
-						chat.append(message);
-						document.querySelector('.chatlogs').appendChild(chat);
-
-							},randomNumberGenerator);
-
-							}
-					}
-						for(var i=0; i<thirdtext.length; i++){
-				if(textArea.trim().toLowerCase() == thirdtext[i]){
-					setTimeout(function(){
-						var photo = document.createElement('div');
-						photo.setAttribute('class','user-photo');
-						var message = document.createElement('p');
-						message.setAttribute('class','chat-message');
-						friendText = document.createTextNode('we have to be discreet')
-						message.appendChild(friendText);
-						var chat = document.createElement('div');
-						chat.setAttribute('class','chat');
-						chat.appendChild(photo);
-						chat.append(message);
-						document.querySelector('.chatlogs').appendChild(chat);
-
-							},randomNumberGenerator);
-
-							}
-					}
-
-					for(var i=0; i<fourthText.length; i++){
-			if(textArea.trim().toLowerCase() == fourthText[i]){
+				for(var i=0; i<refusingText.length; i++){
+			if(textArea.trim().toLowerCase() == refusingText[i]){
 				setTimeout(function(){
 					var photo = document.createElement('div');
 					photo.setAttribute('class','user-photo');
 					var message = document.createElement('p');
 					message.setAttribute('class','chat-message');
-					friendText = document.createTextNode('My address is 1112 2nd St, Sacramento, CA 95814. I hope you make it');
+					friendText = document.createTextNode('you suck')
 					message.appendChild(friendText);
 					var chat = document.createElement('div');
 					chat.setAttribute('class','chat');
 					chat.appendChild(photo);
 					chat.append(message);
 					document.querySelector('.chatlogs').appendChild(chat);
-
-						},randomNumberGenerator);
-
+							
+						},2000);
 						var login = document.createElement('p');
 		login.setAttribute('class','login');
 		loginText = document.createTextNode('user99 left the room');
 		login.appendChild(loginText);
 		setTimeout(function(){
 			document.querySelector('.chatlogs').appendChild(login);
-		},3300);
-			return;
-						}
+		},3000);
+					
+					}
 				}
 
 
 
 
+				talkingGenerator(firsttext,'free tonight?');
+
+				talkingGenerator(secondtext,'where do you live?');
+
+				talkingGenerator(thirdtext,'Pefect! We have to be discreet tho','tell me you live in sac too');
 
 
 
 
 
-		})
+				function talkingGenerator(order,text){
 
+					for(var i=0; i<order.length; i++){
+						if(textArea.trim().toLowerCase() == order[i]){
+					setTimeout(function(){
+						var photo = document.createElement('div');
+						photo.setAttribute('class','user-photo');
+						var message = document.createElement('p');
+						message.setAttribute('class','chat-message');
+						friendText = document.createTextNode(text)
+						message.appendChild(friendText);
+						var chat = document.createElement('div');
+						chat.setAttribute('class','chat');
+						chat.appendChild(photo);
+						chat.append(message);
+						document.querySelector('.chatlogs').appendChild(chat);
+								
+							},randomNumberGenerator);
+						}
+								
+					}
+
+				}
+
+	})
 
 
 
 });
+
+
+
+
+
